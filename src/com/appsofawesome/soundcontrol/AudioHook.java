2b1b5b7dac53117b1d7b5280d823e6a0a227a18a
Stephen Rodriguez <lord.of.stuff@gmail.com>
2014-03-29 19:33:34 -0400

    trying to integrate code.

--- src/com/appsofawesome/soundcontrol/AudioHook.java
+++ src/com/appsofawesome/soundcontrol/AudioHook.java
@@ -1,6 +1,8 @@
 package com.appsofawesome.soundcontrol;
 
 
+import java.lang.reflect.Field;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,6 +12,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.*;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findField;
 
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
@@ -89,8 +92,10 @@ public class AudioHook implements IXposedHookLoadPackage{
 //			if (isRingtoneChangeOn()) {
 				XposedBridge.log("This is where I would change the ringtone.");
 				// http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android-apps/4.4.2_r1/com/android/phone/CallNotifier.java#CallNotifier.onCustomRingQueryComplete%28com.android.internal.telephony.Connection%29
+				Field r = findField(findClass("com.android.phone.CallNotifier", ((Context) param.thisObject).getClassLoader()), "mRinger");
 				//			Ringer r = mRinger;
 				//			r.setCustomRingtoneUri(ci.contactRingtoneUri);
+				findMethod
 //			}
 		}
 
